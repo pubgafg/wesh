66cd6ac073d929f896f60f9e6daa4129235fb962
pubgafg <pubgafg82@gmail.com>
2025-09-07 07:42:20 +0430

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,19 @@
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import PostAd from './pages/PostAd'
-import MyAds from './pages/MyAds'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PostAd from "./pages/PostAd";
+import AdDetails from "./pages/AdDetails";
 
-export default function App(){
+function App() {
   return (
-    <div>
-      <Navbar />
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/post" element={<PostAd />} />
-        <Route path="/my-ads" element={<MyAds />} />
-        <Route path="*" element={<div className="container"><div className="card">Not found</div></div>} />
+        <Route path="/ad/:id" element={<AdDetails />} />
       </Routes>
-    </div>
-  )
+    </BrowserRouter>
+  );
 }
+
+export default App;
