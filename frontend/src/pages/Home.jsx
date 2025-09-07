5211c8c300ce82aae18df7db4dd5690983255b46
pubgafg <pubgafg82@gmail.com>
2025-09-07 07:40:09 +0430

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import AdsList from "../components/AdsList";
 
 function Home() {
   return (
     <div>
-      <h1>خانه</h1>
+      <h1>ویش – اپ شبیه دیوار</h1>
+      <Link to="/post">+ ثبت آگهی</Link>
       <AdsList />
     </div>
   );
